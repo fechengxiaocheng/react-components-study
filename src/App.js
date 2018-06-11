3cf5a746c745b18e631fa5c167293c21feb5da22
zhongan <zhongan@zhongandeMacBook-Pro-6.local>
2018-06-11 14:00:50 +0800

    feat: init switch component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Switch from './components/Switch';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Switch.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Switch extends React.Component {
+    render() {
+        return  (
+            <div className="c-switch">
+                <input typ="checkbox"/> 
+                <span className="c-slider"></span>
+            </div>
+        )
+       
+    }
+}
+
+export default Switch;
+
+
